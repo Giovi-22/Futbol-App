00accd0cf69d48be84ef8bf076cbc3f70c50015c
Giovanni <giovannibarolin@gmail.com>
2023-06-26 00:37:02 -0300

    adding competitions page to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './components/Pages/Home/home.component'
+import { CompetitionsComponent } from './components/Pages/Competitions/competitions.component'
 
 const routes: Routes = [
 	{
 		path: '',
 		component:HomeComponent,
+	},
+	{
+		path: 'competitions',
+		component:CompetitionsComponent,
 	},
 	{
 		path: '**',
