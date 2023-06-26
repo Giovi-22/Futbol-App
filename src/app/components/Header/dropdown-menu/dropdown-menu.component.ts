0b4be9c71151c210d11a9b242dfd71652e54ec76
Giovanni <giovannibarolin@gmail.com>
2023-06-26 03:57:52 -0300

    adding dropdown menu to header component

--- src/app/components/Header/dropdown-menu/dropdown-menu.component.ts
+++ src/app/components/Header/dropdown-menu/dropdown-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompetitionCard } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-dropdown-menu',
@@ -9,7 +10,86 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./dropdown-menu.component.scss']
 })
 export class DropdownMenuComponent implements OnInit {
-
+  competitions:CompetitionCard[] = [
+    {
+      name:"FIFA World Cup",
+      image:"https://crests.football-data.org/qatar.png",
+      code:"WC",
+      id:2000
+    },
+    {
+      name:"Premier League",
+      image:"https://crests.football-data.org/PL.png",
+      code:"PL",
+      id:2021
+    },
+    {
+      name:"Campeonato Brasileiro Série A",
+      image:"https://crests.football-data.org/764.svg",
+      code:"BSA",
+      id:213
+    },
+    {
+      name:"UEFA Champions League",
+      image:"https://crests.football-data.org/CL.png",
+      code:"CL",
+      id:2001
+    },
+    {
+      name:"Bundesliga",
+      image:"https://crests.football-data.org/BL1.png",
+      code:"BL1",
+      id:2002
+    },
+    {
+      name:"Eredivisie",
+      image:"https://crests.football-data.org/ED.png",
+      code:"DED",
+      id:2003
+    },
+    {
+      name:"Primera Division",
+      image:"https://crests.football-data.org/PD.png",
+      code:"PD",
+      id:2014
+    },
+    {
+      name:"Ligue 1",
+      image:"https://crests.football-data.org/FL1.png",
+      code:"FL1",
+      id:2015
+    },
+    {
+      name:"Championship",
+      image:"https://crests.football-data.org/ELC.png",
+      code:"ELC",
+      id:2016
+    },
+    {
+      name:"Primeira Liga",
+      image:"https://crests.football-data.org/PPL.png",
+      code:"PPL",
+      id:2017
+    },
+    {
+      name:"European Championship",
+      image:"https://crests.football-data.org/EUR.svg",
+      code:"EC",
+      id:2018
+    },
+    {
+      name:"Serie A",
+      image:"https://crests.football-data.org/SA.png",
+      code:"SA",
+      id:2019
+    },
+    {
+      name:"Copa Libertadores",
+      image:"https://crests.football-data.org/CLI.svg",
+      code:"CLI",
+      id:2152
+    }
+]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/Header/header/header.component.ts
+++ src/app/components/Header/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { DropdownMenuComponent } from '../dropdown-menu/dropdown-menu.component';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,NavBarComponent],
+  imports: [CommonModule,NavBarComponent,DropdownMenuComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
