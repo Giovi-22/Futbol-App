28d5d85c55ce5ddfe026f4716ab47ba88aa54e38
Giovanni <giovannibarolin@gmail.com>
2023-06-26 01:10:41 -0300

    adding competition card to home page

--- src/app/components/Pages/Home/home.component.ts
+++ src/app/components/Pages/Home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompetitionCardComponent } from '../../Cards/competitionCard/competitionCard.component';
+import { CompetitionCard } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CompetitionCardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  competitions:CompetitionCard[] = [
+    {
+      name:"Premier League",
+      image:"https://crests.football-data.org/PL.png",
+      code:"PL",
+      id:2021
+    },
+    {
+      name:"Campeonato Brasileiro Série A",
+      image:"https://crests.football-data.org/764.svg",
+      code:"BSA",
+      id:213
+    },
+    {
+      name:"UEFA Champions League",
+      image:"https://crests.football-data.org/CL.png",
+      code:"CL",
+      id:2001
+    },
+    {
+      name:"Copa Libertadores",
+      image:"https://crests.football-data.org/CLI.svg",
+      code:"CLI",
+      id:2152
+    }
+]
   constructor() { }
 
   ngOnInit(): void {
