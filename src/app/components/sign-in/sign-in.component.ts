1f1d26875a17405a92f7e32969a4a8a528ee1ded
Giovanni <giovannibarolin@gmail.com>
2023-07-03 10:39:17 -0300

    feat: creating sign-in component

---
+++ src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
