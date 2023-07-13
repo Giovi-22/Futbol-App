ebe3ae5e73a29eb7b3d34e0b8dd8a0a9febe751c
Giovanni <giovannibarolin@gmail.com>
2023-07-13 18:54:21 -0300

    feat: creating spinner component

--- src/app/components/Pages/Competitions/competition-banner/competition-banner.component.ts
+++ src/app/components/Pages/Competitions/competition-banner/competition-banner.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompetitionManagerService } from 'src/app/services/managers/competition-manager.service';
 import { CompetitionEntity } from 'src/app/models/entities/CompetitionEntity';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-competition-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SpinnerComponent],
   templateUrl: './competition-banner.component.html',
   styleUrls: ['./competition-banner.component.scss']
 })
