876a783386dd9db07a0405c6ec598c93651369fc
Giovanni <giovannibarolin@gmail.com>
2023-07-20 08:48:03 -0300

    feat: creating a form control group to manage the email and password inputs

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  formSession!:FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formSession = this.fb.group({
+      email:[""],
+      password:[""]
+    })
+  }
+
+  onSubmit(){
+    console.log("valores del form: ",this.formSession.value)
   }
 
 }
