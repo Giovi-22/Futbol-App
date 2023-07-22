f314d6255ddb7122891606571fda4fae86650c5b
Giovanni <giovannibarolin@gmail.com>
2023-07-22 05:25:48 -0300

    feat: creating signUp component

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+
+  signUpForm!:FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(8)]]
+    })
+  }
+
+}
