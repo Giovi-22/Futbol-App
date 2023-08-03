efc4abe69a5132e31396adc9a0b3c47d018925b3
Giovanni <giovannibarolin@gmail.com>
2023-08-03 19:15:33 -0300

    adding url variables to enviroment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
 	production: true,
+	api_url:"https://api.football-data.org/v4/competitions"
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
 	production: false,
+	api_url:"https://api.football-data.org/v4/competitions",
+	api_server:"https://futbolapp-server-3r9p-dev.fl0.io"
 }
