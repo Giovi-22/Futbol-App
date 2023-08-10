9370de10b6f9f1faed23ee3b5208febc8daffb90
Giovanni <giovannibarolin@gmail.com>
2023-08-09 23:26:14 -0300

    update enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	api_footballData_url:"https://api.football-data.org/v4",
 	api_footballData_token: "860f9df0ee73439a9cc24ca71319e092",
-	api_futbolServer_url:"https://futbolapp-server-v2-bav0-dev.fl0.io/api",
+	api_futbolServer_url:"https://futbolapp-server-3r9p-dev.fl0.io/api",
 	api_competitionStrategy:"CompetitionFutbolApp",	//'CompetitionfootballApi' || 'CompetitionFutbolApp'
 	api_teamStrategy: 'TeamServer' //'TeamServer' || 'TeamfootballApi'
 }
